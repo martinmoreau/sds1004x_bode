49a242e9b1fb328a7a6b8fc7a38875347ccc02a3
Martin Moreau <martin.moreau@strypes.nl>
2021-08-06 16:06:25 +0200

    add SDG1050 support

--- sds1004x_bode/awgdrivers/SDG1050.py
+++ sds1004x_bode/awgdrivers/SDG1050.py
@@ -111,7 +111,7 @@ class SDG1050(BaseAWG):
         if self.output_on:
             self.send_command("C1:OUTP ON")
         else:
-            self.send_command("C1:OUTP OFF")
+            self.send_command("C1:OUTP ON")
 
     def set_frequency(self, channel, freq):
         """
